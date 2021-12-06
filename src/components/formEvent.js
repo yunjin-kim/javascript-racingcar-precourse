df10843a496c6f992cbd7af0d92f242063d9c774
(yunjin) <sion11290805@gmail.com>
2021-12-06 13:45:48 +0900

    refactor: formEvent생성자함수 -> Render class로

--- src/components/formEvent.js
+++ src/components/formEvent.js
@@ -1,78 +1,85 @@
-import { Car } from '../index.js'; 
+import Car from '../index.js'; 
 import { CARS_NAME, GMAE_COUNT, NUMBER, MESSAGE, ELEMENT_MESSAGE } from '../utils/constant.js';
 import { $all, setGameElemetStyle } from '../utils/dom.js';
 
-export function formEvent() {
-  const allForm = $all("form");
-  setGameElemetStyle(ELEMENT_MESSAGE.BEFORE_GAME);
-  for (const eachForm of allForm) {
-    eachForm.addEventListener("submit", (submitEvent) => {
-      submitEvent.preventDefault();
-      if (inputEvent(submitEvent)) {
-        new Car(inputEvent(submitEvent));
-      }
-    })
+export default class Render {
+  constructor(userInput) {
+    this.userInput = userInput;
+    this.allForm = $all("form");
+    this.allInput = $all("input");
+    this.inputValueArray = [];
+    this.formEvent();
   }
-}
 
-function inputEvent(submitEvent) {
-  const allInput = $all("input");
-  const inputValueArray = [];
-  for (const eachInput of allInput) {
-    inputValueArray.push(eachInput.value);
-  }
-  const inputCheckResult = inputCheck(inputValueArray, submitEvent);
-  if (inputCheckResult) {
-    return inputValueArray;
-  }
-}
-
-function inputCheck(inputValueArray, submitEvent) {
-  let gameCountCheckResult;
-  if (submitEvent.target.id === "racing-count-form") {
-    gameCountCheckResult = gameCountCheck(inputValueArray[GMAE_COUNT]);
-  }
-  if (carNameCheck(inputValueArray[CARS_NAME]) && gameCountCheckResult) {
-    setGameElemetStyle(ELEMENT_MESSAGE.COUNT_INPUT);
-    return gameCountCheckResult;
-  }
-}
-
-function carNameCheck(carsName) {
-  let result = true;
-  if (!carsName) {
-    alert(MESSAGE.CARNAME_EMPTY);
-    result = false;
-  } else if (!carsNameLengthCheck(carsName)) {
-    alert(MESSAGE.CARNMAE_OVERFIVEWORD);
-    result = false;
-  }
-  if (result) {
-    setGameElemetStyle(ELEMENT_MESSAGE.CARS_INPUT)
-    return result;
-  }
-}
-
-function carsNameLengthCheck(carsName) {
-  let result = true;
-  for (const carName of carsName.split(',')) {
-    if (carName.length > NUMBER.FIVE) {
-      result = false;
+  formEvent() {
+    // setGameElemetStyle(ELEMENT_MESSAGE.BEFORE_GAME);
+    for (const eachForm of this.allForm) {
+      eachForm.addEventListener("submit", (submitEvent) => {
+        submitEvent.preventDefault();
+        if (inputEvent(submitEvent)) {
+          new Car(this.inputEvent(submitEvent));
+        }
+      })
     }
   }
-  
-  return result;
-}
 
-function gameCountCheck(gameCount) {
-  let result = true;
-  if (!gameCount) {
-    alert(MESSAGE.GAMECOUNT_EMPTY);
-    result = false;
-  } else if (gameCount <= NUMBER.ZERO) {
-    alert(MESSAGE.GAMECOUNT_UNDERONE)
-    result = false;
+  inputEvent(submitEvent) {;
+    for (const eachInput of this.allInput) {
+      this.inputValueArray.push(eachInput.value);
+    }
+    const inputCheckResult = this.inputCheck(submitEvent);
+    if (inputCheckResult) {
+      return this.inputValueArray;
+    }
   }
 
-  return result;
+  inputCheck(submitEvent) {
+    let gameCountCheckResult;
+    if (submitEvent.target.id === "racing-count-form") {
+      gameCountCheckResult = this.gameCountCheck(this.inputValueArray[GMAE_COUNT]);
+    }
+    if (this.carNameCheck(this.inputValueArray[CARS_NAME]) && gameCountCheckResult) {
+      // setGameElemetStyle(ELEMENT_MESSAGE.COUNT_INPUT);
+      return gameCountCheckResult;
+    }
+  }
+
+  gameCountCheck(gameCount) {
+    let result = true;
+    if (!gameCount) {
+      alert(MESSAGE.GAMECOUNT_EMPTY);
+      result = false;
+    } else if (gameCount <= NUMBER.ZERO) {
+      alert(MESSAGE.GAMECOUNT_UNDERONE)
+      result = false;
+    }
+  
+    return result;
+  }
+
+  carNameCheck(carsName) {
+    let result = true;
+    if (!carsName) {
+      alert(MESSAGE.CARNAME_EMPTY);
+      result = false;
+    } else if (!this.carsNameLengthCheck(carsName)) {
+      alert(MESSAGE.CARNMAE_OVERFIVEWORD);
+      result = false;
+    }
+    if (result) {
+      // setGameElemetStyle(ELEMENT_MESSAGE.CARS_INPUT)
+      return result;
+    }
+  }
+
+  carsNameLengthCheck(carsName) {
+    let result = true;
+    for (const carName of carsName.split(',')) {
+      if (carName.length > NUMBER.FIVE) {
+        result = false;
+      }
+    }
+    
+    return result;
+  }
 }

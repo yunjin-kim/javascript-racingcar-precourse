519a3b6390d0a303b4b065a7aa3fc9d85afba1d6
(yunjin) <sion11290805@gmail.com>
2021-12-06 13:29:40 +0900

    refactor: Car생성자함수 -> Car class로

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import { gameStart } from './components/game.js';
 import { NUMBER, WINNER } from './utils/constant.js';
 import { $, $new } from './utils/dom.js';
 
-export function Car(userInput) {
-  this.userInput = userInput;
-  const gameResultWrap = $("racing-result");
-  const gameResultDiv = $new("div");
-  const gameResultSpan = $new("span");
-  const gameResult = gameStart(userInput);
-  gameResultSpan.id = "racing-winners";
-  gameResultSpan.style.opacity = NUMBER.ZERO;
-  gameResultDiv.innerHTML = `${gameResult.gameProcess}\n ${WINNER}: ${gameResult.gmaeWinner} `;
-  gameResultSpan.textContent = gameResult.gmaeWinner;
-  gameResultWrap.append(gameResultDiv, gameResultSpan);
+export default class Car {
+  constructor(userInput) {
+    this.userInput = userInput;
+    this.gameResultWrap = $("racing-result");
+    this.gameResultDiv = $new("div");
+    this.gameResultSpan = $new("span");
+    this.gameResult = gameStart(userInput);
+    this.gameResultSpan.id = "racing-winners";
+    this.gameResultSpan.style.opacity = NUMBER.ZERO;
+    gameResultDiv.innerHTML = `${gameResult.gameProcess}\n ${WINNER}: ${gameResult.gmaeWinner} `;
+    gameResultSpan.textContent = gameResult.gmaeWinner;
+    gameResultWrap.append(gameResultDiv, gameResultSpan);
+  } 
 }
 
 formEvent()
